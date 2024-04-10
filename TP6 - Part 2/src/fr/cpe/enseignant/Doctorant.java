bf6ce4bfc1a39ca1dbfd8a5af4042ceb975cd25f
Charles Berthet <138251887+CharlesBerthet@users.noreply.github.com>
2024-04-10 09:49:55 +0200

    Doctorant

--- TP6 - Part 2/src/fr/cpe/enseignant/Doctorant.java
+++ TP6 - Part 2/src/fr/cpe/enseignant/Doctorant.java
@@ -1,11 +1,32 @@
 package fr.cpe.enseignant;
 
-public class Doctorant extends Enseignant{
+import fr.cpe.salaire.ISalaire;
+
+public class Doctorant extends Enseignant implements ISalaire {
 
     public static final int NBR_HEURES = 100;
     public static final int SALAIRE_BRUT = 40;
 
     public Doctorant(String nom, String prenom, SpecialitesEnseignement specialite, int nbrHeures) {
         super(nom, prenom, specialite, nbrHeures);
+    }
+
+
+    @Override
+    public double salaire() {
+        if (getNbrHeures() <= NBR_HEURES) {
+            return getNbrHeures() * SALAIRE_BRUT;
+        }
+        return NBR_HEURES * SALAIRE_BRUT;
+    }
+
+    @Override
+    public double charge() {
+        return salaire() * CHARGE_PATRONAL;
+    }
+
+    @Override
+    public double getTotal() {
+        return salaire() + charge();
     }
 }
